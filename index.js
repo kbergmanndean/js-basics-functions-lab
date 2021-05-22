e8363bba7aa2984ed97b999220a2ba05b9b80006
Kaia <kbergmanndean@gmail.com>
2021-05-22 14:37:02 -0400

    commit tree 4e998b377c335819ea7bea14a92c5a508112a1ae

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+    let distance
+    if(pickup>=42) {
+       distance = (pickup-42)
+    } else {
+        distance = (42-pickup)
+    }
+return distance
+}
+
+function distanceFromHqInFeet(pickup) {
+    let blocks = distanceFromHqInBlocks(pickup);
+    return blocks*264
+}
+
+function distanceTravelledInFeet(pointA,pointB) {
+    let distance = Math.abs((pointB-pointA)*264)
+    return distance
+}
+function calculatesFarePrice(pointA,pointB){
+    let distance= distanceTravelledInFeet(pointA,pointB)
+    let fare
+    if (distance>400 && distance<2000) {
+        fare=(distance*.02)-(400*.02)
+        // let decFare=fare.toFixed(2);
+        // return decFare
+        return fare
+    }
+}
